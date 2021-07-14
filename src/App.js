f0c0133cea2112105a7710564454176d83cc7bd6
Mateusz Choma <kontakt@coderoad.pl>
2021-07-14 19:32:18 +0200

    Fetching data - failed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
 
-export const App = () => {
-  return (
-    <div>
-      CodeRoad APP
-    </div>
-  )
+export class App extends React.Component {
+  state = {
+    data: null
+  }
+
+  async componentDidMount () {
+    const response = await fetch('https://randomuser.me/api')
+    const data = await response.json()
+    this.setState(() => ({
+      data: data
+    }))
+  }
+
+  render () {
+    return (
+      <div>
+        CodeRoad APP
+        {this.state.data}
+      </div>
+    )
+  }
 }
 
 export default App
